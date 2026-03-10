7df4068599b9f19cad82c590ae3a3230301c2eb9
Marcikadraga <git@stackblitz.com>
2026-03-10 07:58:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# interviewPractise
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Marcikadraga/interviewPractise)
---
+++ script.js
@@ -0,0 +1,111 @@
+/*
+1️⃣Create a new array that contains only the names of users older than 18.
+*/
+const users = [
+  { name: "Anna", age: 17 },
+  { name: "John", age: 25 },
+  { name: "Peter", age: 30 },
+  { name: "Kate", age: 15 }
+];
+
+
+
+/*
+2️⃣Remove Duplicates
+Write a function:
+*/
+const arr=[1, 2, 2, 3, 4, 4, 5];
+
+function removeDuplicates(arr) {
+
+}
+
+
+
+
+/*
+3️⃣ Count Characters
+Write a function that counts how many times each character appears.
+countChars("hello");
+Expected output:
+{
+h:1,
+e:1,
+l:2,
+o:1
+}
+*/
+
+
+
+/*
+4️⃣ Object Counter
+
+Complete the object so it works like a counter.
+
+const counter = {
+value: 0,
+
+increment() {
+
+},
+
+decrement() {
+
+},
+
+reset() {
+
+}
+};
+
+counter.increment()
+counter.increment()
+counter.decrement()
+
+console.log(counter.value) // 1
+*/
+
+
+
+
+/*
+5️⃣ Find the Largest Number
+
+Write a function:
+
+function findMax(arr) {
+
+}
+
+Example:
+
+findMax([4, 2, 9, 1, 7])
+*/
+
+
+
+/*
+6️⃣ Reverse a String
+
+Write a function:
+
+function reverseString(str) {
+
+}
+
+Example:
+
+reverseString("epam")
+
+Output:
+
+"mape"
+*/
+
+
+
+
+
+
+
