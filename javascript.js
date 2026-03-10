61d80521c336aececca9a6973e80da66f4ee4bb5
Marcikadraga <marciprog@gmail.com>
2026-03-10 10:11:09 +0100

    refactor the unsolved task

---
+++ javascript.js
@@ -0,0 +1,173 @@
+/*
+1️⃣
+Create a new array that contains only the names of users older than 18.
+*/
+const users = [
+    { name: "Anna", age: 17 },
+    { name: "John", age: 25 },
+    { name: "Peter", age: 30 },
+    { name: "Kate", age: 15 }
+  ];
+
+
+
+/*
+2️⃣ Remove Duplicates
+Write a function:
+*/
+const arr=[1, 2, 2, 3, 4, 4, 5];
+function removeDuplicates(arr) {
+
+}
+
+
+
+
+
+
+
+
+/*
+3️⃣ Count Characters
+Write a function that counts how many times each character appears.
+{
+  h:1,
+  e:1,
+  l:2,
+  o:1
+}
+*/
+let word= "hello";
+function countChars(word){
+
+}
+
+
+
+
+
+/*
+4️⃣ Object Counter
+Complete the object so it works like a counter.
+need a value,increment(),decrement(),reset()
+
+counter.increment()
+counter.increment()
+counter.decrement()
+console.log(counter.value) // 1
+*/
+
+
+
+
+
+/*5️⃣ Find the Largest Number
+Write a function:
+
+function findMax(arr) {
+
+}
+Example:
+
+findMax([4, 2, 9, 1, 7])
+Expected output:
+*/
+
+
+
+
+
+
+
+
+/*
+6️⃣ Reverse a String
+Write a function:
+function reverseString(str) {
+
+}
+Example:
+reverseString("epam")
+Output:
+"mape"
+*/
+
+
+
+
+
+
+/*
+7️⃣ Sum of Array
+Write a function that returns the sum of all numbers.
+
+function sumArray(arr) {
+
+}
+Example:
+
+sumArray([1,2,3,4])
+Output:
+10
+*/
+
+
+
+
+/*
+8️⃣ this Keyword Question
+What will this code print?
+
+const obj = {
+  name: "EPAM",
+  showName() {
+    console.log(this.name);
+  }
+};
+
+obj.showName();
+Then modify the code so that showName still works even if you do:
+
+const fn = obj.showName;
+fn();
+*/
+
+
+
+
+
+
+/*
+9️⃣ Closure Task
+Write a function:
+function createCounter() {
+
+}
+Usage:
+const counter = createCounter();
+counter(); // 1
+counter(); // 2
+counter(); // 3
+Hint: this is a closure task.
+*/
+
+
+
+
+
+
+/*
+🔟 Group Array Values
+Write a function that groups numbers by even and odd.
+
+groupNumbers([1,2,3,4,5,6])
+Expected output:
+
+{
+  even: [2,4,6],
+  odd: [1,3,5]
+}
+*/
+
+
+
